771e1bd4e9c45f4c2502f0791ba437321487279b
Dharanish-sk <156311583+Dharanish-sk@users.noreply.github.com>
2025-04-21 08:18:31 +0530

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,8 @@
+// Error: no-unused-vars
+const name = "Sai";  // ESLint says: you're not using this anywhere
+
+// Error: eqeqeq
+if (a == b) { ... }  // ESLint says: use `===` instead
+
+// Error: no-console
+console.log("Debug info");  // ESLint says: remove in production
